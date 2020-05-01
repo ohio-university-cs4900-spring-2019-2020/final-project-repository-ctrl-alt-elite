138496cff933f55cdf44334fb664208a112f2eb7
Jacob Frommeyer <43117957+jacobfrommeyer@users.noreply.github.com>
2020-05-01 19:36:01 -0400

    Added Features
    Added bullet firing, generated random targets, added new models(cannot get assets to import on models)

--- modules/final_project/src/GLViewfinal_project.h
+++ modules/final_project/src/GLViewfinal_project.h
@@ -1,20 +1,13 @@
 #pragma once
 
 #include "GLView.h"
-#include "box.h"
-#include "target.h"
-#include "gun.h"
-#include "bullet.h"
-#include <WOGUILabel.h>
-#include <WOFTGLString.h>
-#include <MGLFTGLString.h>
 
 namespace Aftr
 {
    class Camera;
 
 /**
-   \class GLViewfinal_project
+   \class GLViewNewModule
    \author Scott Nykl 
    \brief A child of an abstract GLView. This class is the top-most manager of the module.
 
@@ -25,41 +18,39 @@ namespace Aftr
     \{
 */
 
-class GLViewfinal_project : public GLView
+class GLViewNewModule : public GLView
 {
 public:
-   static GLViewfinal_project* New( const std::vector< std::string >& outArgs );
-   virtual ~GLViewfinal_project();
+   static GLViewNewModule* New( const std::vector< std::string >& outArgs );
+   virtual ~GLViewNewModule();
    virtual void updateWorld(); ///< Called once per frame
    virtual void loadMap(); ///< Called once at startup to build this module's scene
-   virtual void createfinal_projectWayPoints();
    virtual void onResizeWindow( GLsizei width, GLsizei height );
    virtual void onMouseDown( const SDL_MouseButtonEvent& e );
    virtual void onMouseUp( const SDL_MouseButtonEvent& e );
    virtual void onMouseMove( const SDL_MouseMotionEvent& e );
    virtual void onKeyDown( const SDL_KeyboardEvent& key );
    virtual void onKeyUp( const SDL_KeyboardEvent& key );
-   virtual void fire_bullet();
 
-   Box* box1;
-   Box* box2;
-   Box* box3;
-   Box* box4;
-   Target* target1;
-   Target* target2;
-   Target* target3;
-   Target* target4;
-   Gun* gun;
-   Bullet* bullet;
 
-   //WOWayPointSpherical* wayPt;
-   //WOGUILabel* testText;
-   int total_hit;
-   //WOGUILabel* testText = WOGUILabel::New(nullptr);
+   //Object creation methods
+   virtual void createNewModuleWayPoints();
+   virtual void createPirateShipModel();
+   virtual void createRaceCarModel();
+   virtual void createPlayersAKModel();
+   virtual void bulletFiredFromGun(WO* Shooter);
+   virtual void WeaponFiring(WO* Shooter);
+   std::vector<size_t> ExistingBulletIDs;
+   virtual void MoveBullet(WO* Bullet);
+   virtual void GenerateRandomTargetHuman(float xCoord, float yCord);
 
 protected:
-   GLViewfinal_project( const std::vector< std::string >& args );
+   GLViewNewModule( const std::vector< std::string >& args );
    virtual void onCreate();   
+   size_t PlayerWeaponWOIndex;
+   
+
+   //Creates the sound manager for the module:
 };
 
 /** \} */
